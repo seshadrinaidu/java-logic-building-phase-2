91dcc9e32650c70fd4a5b13741a9d53e26e3a699
Seshadri Naidu <seshadrinaidu454@gmail.com>
2026-07-06 19:44:48 +0530

    Pattern 2 : Print 4 stars

---
+++ src/Level4_Star_Number_Triangle_Patterns/Pattern02_PrintFourStars.java
@@ -0,0 +1,40 @@
+/*
+Problem 2: Print Four Stars (****)
+
+Understanding the Problem:
+-> I need to print four stars (****).
+-> There is no user input.
+-> The output is fixed.
+-> No iterations are required.
+
+Choosing the Approach:
+-> Since the output is constant,
+   a simple System.out.print() statement is enough.
+-> No loop is required for this problem.
+
+Dry Run:
+
+System.out.print("****");
+
+Output:
+****
+
+Program ends.
+
+Edge Cases:
+-> No user input is involved.
+-> No boundary conditions.
+-> No invalid input cases.
+
+Review Solution:
+-> This is another basic pattern printing problem.
+-> The output is fixed, so loops are unnecessary.
+-> It prepares us for the next problem where the number of stars becomes dynamic and loops are introduced.
+*/
+package Level4_Star_Number_Triangle_Patterns;
+
+public class Pattern02_PrintFourStars {
+    public static void main(String[] args){
+        System.out.print("****");
+    }
+}
