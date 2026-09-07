e79dac54ad27d2d3ad6c312b63185dac79178445
Seshadri Naidu <seshadrinaidu454@gmail.com>
2026-09-07 17:32:54 +0530

    Problem 27:repace Negative Number with Zero

---
+++ src/level6_BasicArrays/Pattern5_ArrayTransformation/Problem27_ReplaceNegativeWithZero.java
@@ -0,0 +1,16 @@
+package level6_BasicArrays.Pattern5_ArrayTransformation;
+
+public class Problem27_ReplaceNegativeWithZero {
+    public static void main(String[] args) {
+     int[] arr={1, -2, 3, -4, 5};
+     for (int i=0;i<arr.length;i++){
+         if (arr[i]>0){
+            continue;
+         }
+         arr[i]=0;
+     }
+     for (int a:arr){
+         System.out.print(a+" ");
+     }
+    }
+}
