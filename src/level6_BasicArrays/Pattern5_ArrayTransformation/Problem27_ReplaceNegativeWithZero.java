33d2dc6e0d7c84f6eb089ae0b1e4115606371d81
Seshadri Naidu <seshadrinaidu454@gmail.com>
2026-09-08 16:25:02 +0530

    Refactor Problem 27 to preserve original array

--- src/level6_BasicArrays/Pattern5_ArrayTransformation/Problem27_ReplaceNegativeWithZero.java
+++ src/level6_BasicArrays/Pattern5_ArrayTransformation/Problem27_ReplaceNegativeWithZero.java
@@ -3,13 +3,12 @@ package level6_BasicArrays.Pattern5_ArrayTransformation;
 public class Problem27_ReplaceNegativeWithZero {
     public static void main(String[] args) {
      int[] arr={1, -2, 3, -4, 5};
+     int[] result=new int[arr.length];
+
      for (int i=0;i<arr.length;i++){
-         if (arr[i]>0){
-            continue;
-         }
-         arr[i]=0;
+         result[i] = Math.max(arr[i], 0);
      }
-     for (int a:arr){
+     for (int a:result){
          System.out.print(a+" ");
      }
     }
